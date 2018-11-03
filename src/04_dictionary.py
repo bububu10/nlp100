d433b4e35f446d0d75a10244c92b6edc255abcd5
bububu10 <t.kiminobu@gmail.com>
2018-11-04 02:11:16 +0900

    fix 04

--- src/04_dictionary.py
+++ src/04_dictionary.py
@@ -12,7 +12,7 @@ def main():
     print(words)
 
     pickups = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-    pickup_indexes = {index - 1 for index in pickups}
+    pickup_indexes = [index - 1 for index in pickups]
     print(pickup_indexes)
 
     chars = {index: word[0] if (index in pickup_indexes) else word[0:2] for index, word in enumerate(words)}
