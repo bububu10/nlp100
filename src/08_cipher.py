4e0eb15450e8a84698c17cc5d186d13b92dd51d0
bububu10 <t.kiminobu@gmail.com>
2018-11-04 10:48:47 +0900

    add 08

---
+++ src/08_cipher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+def cipher(plain: str) -> str:
+    seq = [chr(219 - ord(char)) if (char.islower()) else char for char in plain]
+    return "".join(seq)
+
+
+def main():
+    origin = 'I am a cat.'
+    print('original: ' + origin)
+    encrypt = cipher(origin)
+    print('encrypt: ' + encrypt)
+    decrypt = cipher(encrypt)
+    print('decrypt: ' + decrypt)
+
+
+if __name__ == '__main__':
+    main()
