9f1dfcacc66f6e165f1d492ebe66dfbef494b363
bububu10 <t.kiminobu@gmail.com>
2018-11-05 00:32:16 +0900

    add 20 21

--- src/03_regeular_expression/20_read_json.py
+++ src/03_regeular_expression/20_read_json.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+import gzip
 import json
 
 
 def main():
-    with open('jawiki-country.json') as file:
+    with gzip.open('jawiki-country.json.gz', mode='rb') as file:
         article = [json.loads(line) for line in file if json.loads(line).get('title') == 'イギリス'][0]
         print(article.get('text'))
+        with open('out/england_article.json', mode='w') as out:
+            json.dump(article, out, ensure_ascii=False)
 
 
 if __name__ == '__main__':
