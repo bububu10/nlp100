cce0814daba54b4db13d70f4c8b2321aca6498d5
bububu10 <t.kiminobu@gmail.com>
2018-11-04 13:44:45 +0900

    fix 11

--- src/02_basic_unix_command/11_replace_tab_to_space.py
+++ src/02_basic_unix_command/11_replace_tab_to_space.py
@@ -2,10 +2,11 @@
 
 
 def main():
-    with open('./out/11_out.txt', mode='w') as out:
-        with open('./hightemp.txt') as file:
-            for line in file:
-                out.write(line.replace('\t', ' '))  # cat hightemp.txt | sed s/$'\t'/' '/g
+    out = open('./out/11_out.txt', mode='w')
+    with open('./hightemp.txt') as file:
+        for line in file:
+            out.write(line.replace('\t', ' '))  # cat hightemp.txt | sed s/$'\t'/' '/g
+    out.close()
 
 
 if __name__ == '__main__':
