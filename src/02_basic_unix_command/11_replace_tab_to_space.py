71887d3450defd1274ea4b9a1e61aba0574c2d5b
bububu10 <t.kiminobu@gmail.com>
2018-11-04 13:24:30 +0900

    fix 11, add 12

--- src/02_basic_unix_command/11_replace_tab_to_space.py
+++ src/02_basic_unix_command/11_replace_tab_to_space.py
@@ -5,11 +5,8 @@ def main():
     with open('./out/11_out.txt', mode='w') as out:
         with open('./hightemp.txt') as file:
             for line in file:
-                out.write(line.replace('\t', ' '))  # cat hightemp.txt | sed s/$'\t'/' '/g > ./out/11_sed_out.txt
+                out.write(line.replace('\t', ' '))  # cat hightemp.txt | sed s/$'\t'/' '/g
 
 
 if __name__ == '__main__':
     main()
-
-
-
---
+++ src/02_basic_unix_command/12_split_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+def main():
+    col1_out = open('./out/col1.txt', mode='w')
+    col2_out = open('./out/col2.txt', mode='w')
+
+    with open('./hightemp.txt') as file:
+        for line in file:
+            cols = line.split('\t')
+            col1_out.write(cols[0] + '\n')  # cat hightemp.txt | cut -f 1
+            col2_out.write(cols[1] + '\n')  # cat hightemp.txt | cut -f 2
+
+
+if __name__ == '__main__':
+    main()
