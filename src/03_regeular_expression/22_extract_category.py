97b176dfa6a504f93a15e6747e6c74ad0c362922
bububu10 <t.kiminobu@gmail.com>
2018-11-05 23:50:10 +0900

    add 22

---
+++ src/03_regeular_expression/22_extract_category.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import json
+import re
+
+
+def main():
+    with open('out/england_article.json') as file:
+        article = json.loads(file.readline())
+        text = article.get('text')
+        # print(text)
+
+    pattern = r'\[\[Category:(.*)\]\]'
+    categories = re.findall(pattern, text)
+    for category in categories:
+        print(category)
+
+
+if __name__ == '__main__':
+    main()
